0dc0ea386a8f9d437465396a0a43af0e52aa0322
YallamrajuLokesh <149151515+YallamrajuLokesh@users.noreply.github.com>
2023-12-20 23:28:54 +0530

    Create Insert_data.sql

---
+++ Insert_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO SAILORS VALUES
+(1, 'Jack',8,25),
+(2, 'Emily',6,22),
+(3, 'Sam',7,28),
+(4, 'Lily',9,30),
+(5, 'Noah',5,21);
+
+INSERT INTO BOATS VALUES
+(101, 'Boat1','Black'),
+(102, 'Boat2','Green'),
+(103, 'Boat3','Blue'),
+(104, 'Boat4','Red'),
+(105, 'Boat5','Green');
+
+INSERT INTO RESERVES VALUES
+(1,101, '2023-01-15'),
+(2,102, '2023-02-20'),
+(3,104, '2023-03-25'),
+(4,105, '2023-04-10'),
+(5,103, '2023-05-05');
